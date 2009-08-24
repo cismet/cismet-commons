e4c73a282b184c3c8c3a8a3e810faa32b6e13442
Stefan Richter <stefan.richter@cismet.de>
2009-08-24 12:31:11 +0000

    Nullpointer Bugfix

--- src/main/java/de/cismet/tools/BlacklistClassloading.java
+++ src/main/java/de/cismet/tools/BlacklistClassloading.java
@@ -26,16 +26,20 @@ public final class BlacklistClassloading {
      * @return the class to load or null if class is not found.
      */
     public static final Class<?> forName(String classname) {
-        final StringBuilder classNameWithLoaderBuiler = new StringBuilder(classname);
-        classNameWithLoaderBuiler.append(Thread.currentThread().getContextClassLoader());
-        if (!blacklist.contains(classNameWithLoaderBuiler.toString())) {
-            try {
-                return Class.forName(classname);
-            } catch (ClassNotFoundException ex) {
-                blacklist.add(classNameWithLoaderBuiler.toString());
-                if (log.isDebugEnabled()) {
-                    log.debug("Could not load class " + classname + "! Added classname to blacklist", ex);
+        if (classname != null) {
+            final StringBuilder classNameWithLoaderBuiler = new StringBuilder(classname);
+            classNameWithLoaderBuiler.append(Thread.currentThread().getContextClassLoader());
+            if (!blacklist.contains(classNameWithLoaderBuiler.toString())) {
+                try {
+                    return Class.forName(classname);
+                } catch (ClassNotFoundException ex) {
+                    blacklist.add(classNameWithLoaderBuiler.toString());
+                    if (log.isDebugEnabled()) {
+                        log.debug("Could not load class " + classname + "! Added classname to blacklist", ex);
+                    }
                 }
+            } else {
+                log.debug("Classname to load was null!");
             }
         }
         return null;
