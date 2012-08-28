96b4dd2242f78280c2e31fe0aa02ac3f82f79e50
Martin Scholl <martin.scholl@cismet.de>
2012-08-28 09:56:03 +0200

    [issue #5] added some useful operations for more convenient usage

--- src/main/java/de/cismet/ext/CExtManager.java
+++ src/main/java/de/cismet/ext/CExtManager.java
@@ -5,10 +5,6 @@
 *              ... and it just works.
 *
 ****************************************************/
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.cismet.ext;
 
 import org.openide.util.Lookup;
@@ -24,7 +20,24 @@ import java.util.Collection;
  */
 public class CExtManager {
 
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new CExtManager object.
+     */
+    private CExtManager() {
+    }
+
     //~ Methods ----------------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public static CExtManager getInstance() {
+        return LazyInitialiser.INSTANCE;
+    }
 
     /**
      * DOCUMENT ME!
@@ -47,5 +60,59 @@ public class CExtManager {
         }
 
         return extensions;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   <T>      DOCUMENT ME!
+     * @param   c        DOCUMENT ME!
+     * @param   context  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     *
+     * @throws  IllegalStateException  DOCUMENT ME!
+     */
+    public <T> T getExtension(final Class<T> c, final CExtContext context) {
+        final Collection<? extends CExtProvider> providers = Lookup.getDefault().lookupAll(CExtProvider.class);
+
+        for (final CExtProvider provider : providers) {
+            // TODO: maybe assignable from would suit better
+            if (c.equals(provider.getValidClass())) {
+                final Collection<? extends T> extensions = provider.provideExtensions(context);
+                if (extensions.size() == 1) {
+                    return extensions.iterator().next();
+                } else if (extensions.size() > 1) {
+                    // TODO: what to do? shall we omit this operation? shall the caller be responsible for choosing the
+                    // right instance? and how will he do that? is it legal to provide more than one extension for the
+                    // same context?
+                    throw new IllegalStateException();
+                }
+            }
+        }
+
+        return null;
+    }
+
+    //~ Inner Classes ----------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @version  $Revision$, $Date$
+     */
+    private static final class LazyInitialiser {
+
+        //~ Static fields/initializers -----------------------------------------
+
+        private static final CExtManager INSTANCE = new CExtManager();
+
+        //~ Constructors -------------------------------------------------------
+
+        /**
+         * Creates a new LazyInitialiser object.
+         */
+        private LazyInitialiser() {
+        }
     }
 }
