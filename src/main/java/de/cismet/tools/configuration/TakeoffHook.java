466790f32e565615f79ddc3ff4b280ffe0655bd7
helllth <th@cismet.de>
2012-09-26 15:11:40 +0200

    cismet/cids-navigator#19 added TakeoffHook Interface

---
+++ src/main/java/de/cismet/tools/configuration/TakeoffHook.java
@@ -0,0 +1,28 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.tools.configuration;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   thorsten
+ * @version  $Revision$, $Date$
+ */
+public interface TakeoffHook {
+
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * This operation is one of the first operations tha application will call.
+     */
+    void applicationTakeoff();
+}
