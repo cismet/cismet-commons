412f22e6d5d74395c55ff2d77c972cd3df853b74
Martin Scholl <martin.scholl@cismet.de>
2013-02-04 15:10:56 +0100

    [issue #12] additional constructor and simple jdoc

--- src/main/java/de/cismet/commons/ref/CacheException.java
+++ src/main/java/de/cismet/commons/ref/CacheException.java
@@ -8,10 +8,10 @@
 package de.cismet.commons.ref;
 
 /**
- * DOCUMENT ME!
+ * This exception shall be used to indicate errors during operations related to caching.
  *
  * @author   mscholl
- * @version  $Revision$, $Date$
+ * @version  1.0
  */
 public class CacheException extends RuntimeException {
 
@@ -20,8 +20,17 @@ public class CacheException extends RuntimeException {
     /**
      * Creates a new CacheException object.
      *
-     * @param  message  DOCUMENT ME!
-     * @param  cause    DOCUMENT ME!
+     * @param  message  a message indicating the reason of the error
+     */
+    public CacheException(final String message) {
+        super(message);
+    }
+
+    /**
+     * Creates a new CacheException object.
+     *
+     * @param  message  a message indicating the reason of the error
+     * @param  cause    the cause of the exception
      */
     public CacheException(final String message, final Throwable cause) {
         super(message, cause);
