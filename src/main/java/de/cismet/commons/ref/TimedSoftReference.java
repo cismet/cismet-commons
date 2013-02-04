9b006ad2e340c6dfbe63389bd5125743f847e655
Martin Scholl <martin.scholl@cismet.de>
2013-02-04 16:03:10 +0100

    [issue #12] added javadoc for TimedSoftReference

--- src/main/java/de/cismet/commons/ref/TimedSoftReference.java
+++ src/main/java/de/cismet/commons/ref/TimedSoftReference.java
@@ -14,10 +14,12 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 /**
- * DOCUMENT ME!
+ * Special implementation of {@link SoftReference} that purges the referent after the given idle time. Idle time in this
+ * case means time without access to the actual referent via {@link #get()}
  *
  * @author   martin.scholl@cismet.de
- * @version  $Revision$, $Date$
+ * @version  1.0
+ * @see      {@link SoftReference}
  */
 public final class TimedSoftReference<T> extends SoftReference<T> {
 
@@ -27,39 +29,55 @@ public final class TimedSoftReference<T> extends SoftReference<T> {
 
     //~ Instance fields --------------------------------------------------------
 
-    private final transient long clearAfterIdleTime;
+    private final transient long purgeAfterIdleTime;
 
     private transient TimerTask currentTask;
 
     //~ Constructors -----------------------------------------------------------
 
     /**
-     * Creates a new TimedSoftReference object.
+     * Creates a new TimedSoftReference object. Actually using this constructor is equal to calling <code>new
+     * TimedSoftReference(referent, null, purgeAfterIdleTime)</code>
      *
-     * @param  referent            DOCUMENT ME!
-     * @param  clearAfterIdleTime  DOCUMENT ME!
+     * @param  referent            the actual value
+     * @param  purgeAfterIdleTime  the time after a non-accessed referent will be removed
+     *
+     * @see    {@link #TimedSoftReference(java.lang.Object, java.lang.ref.ReferenceQueue, long)}
      */
-    public TimedSoftReference(final T referent, final long clearAfterIdleTime) {
-        this(referent, null, clearAfterIdleTime);
+    public TimedSoftReference(final T referent, final long purgeAfterIdleTime) {
+        this(referent, null, purgeAfterIdleTime);
     }
 
     /**
      * Creates a new TimedSoftReference object.
      *
-     * @param  referent            DOCUMENT ME!
-     * @param  q                   DOCUMENT ME!
-     * @param  clearAfterIdleTime  DOCUMENT ME!
+     * @param  referent            the actual value
+     * @param  q                   a reference queue to be used
+     * @param  purgeAfterIdleTime  the time after a non-accessed referent will be removed
+     *
+     * @see    {@link SoftReference}
+     * @see    {@link #get()}
      */
-    public TimedSoftReference(final T referent, final ReferenceQueue<? super T> q, final long clearAfterIdleTime) {
+    public TimedSoftReference(final T referent, final ReferenceQueue<? super T> q, final long purgeAfterIdleTime) {
         super(referent, q);
 
-        this.clearAfterIdleTime = clearAfterIdleTime;
+        this.purgeAfterIdleTime = purgeAfterIdleTime;
 
         reschedule();
     }
 
     //~ Methods ----------------------------------------------------------------
 
+    /**
+     * Gets the actual value of this {@link SoftReference} (the referent). The actual value may not be present anymore
+     * for reasons explained in <code>SoftReference</code> javadoc or because the value has not been accessed for <code>
+     * purgeAfterIdleTime</code> milliseconds. As soon as the value has been accessed (and the referent is still
+     * existing) the purge operation is rescheduled to take place after <code>purgeAfterIdleTime</code> milliseconds.
+     *
+     * @return  the actual value (the referent) or <code>null</code> if it has been cleaned for any reason
+     *
+     * @see     {@link SoftReference#get()}
+     */
     @Override
     public T get() {
         final T obj = super.get();
@@ -77,17 +95,18 @@ public final class TimedSoftReference<T> extends SoftReference<T> {
     }
 
     /**
-     * DOCUMENT ME!
+     * Reschedules the clear timer task. Should only be called by the {@link #get()} operation. Otherwise
+     * synchronization has to be done.
      */
     private void reschedule() {
         currentTask = new ClearTimerTask();
-        TIMER.schedule(currentTask, clearAfterIdleTime);
+        TIMER.schedule(currentTask, purgeAfterIdleTime);
     }
 
     //~ Inner Classes ----------------------------------------------------------
 
     /**
-     * DOCUMENT ME!
+     * Simple timer task that clears the referent.
      *
      * @version  $Revision$, $Date$
      */
