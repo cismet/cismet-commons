3f27b9a7d4fbbf70d14e1ee121f48fbca8c5b054
Martin Scholl <martin.scholl@cismet.de>
2013-02-04 17:12:36 +0100

    [issue #12] TimedSoftReferenceTest jalopy format

--- src/test/java/de/cismet/commons/ref/TimedSoftReferenceTest.java
+++ src/test/java/de/cismet/commons/ref/TimedSoftReferenceTest.java
@@ -1,52 +1,81 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
 package de.cismet.commons.ref;
-
-
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.Test;
 import org.junit.BeforeClass;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
+ * DOCUMENT ME!
  *
- * @author martin.scholl@cismet.de
+ * @author   martin.scholl@cismet.de
+ * @version  $Revision$, $Date$
  */
-public class TimedSoftReferenceTest
-{
+public class TimedSoftReferenceTest {
 
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @throws  Exception  DOCUMENT ME!
+     */
     @BeforeClass
-    public static void setUpClass() throws Exception
-    {
+    public static void setUpClass() throws Exception {
     }
 
+    /**
+     * DOCUMENT ME!
+     *
+     * @throws  Exception  DOCUMENT ME!
+     */
     @AfterClass
-    public static void tearDownClass() throws Exception
-    {
+    public static void tearDownClass() throws Exception {
     }
 
+    /**
+     * DOCUMENT ME!
+     */
     @Before
-    public void setUp()
-    {
+    public void setUp() {
     }
 
+    /**
+     * DOCUMENT ME!
+     */
     @After
-    public void tearDown()
-    {
+    public void tearDown() {
     }
 
-    private String getCurrentMethodName()
-    {
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    private String getCurrentMethodName() {
         return new Throwable().getStackTrace()[1].getMethodName();
     }
 
+    /**
+     * DOCUMENT ME!
+     *
+     * @throws  Exception  DOCUMENT ME!
+     */
     @Test
-    public void testGet() throws Exception
-    {
+    public void testGet() throws Exception {
         System.out.println("TEST " + getCurrentMethodName());
-        
-        TimedSoftReference tsr = new TimedSoftReference(new Object(), 200);
+
+        final TimedSoftReference tsr = new TimedSoftReference(new Object(), 200);
         Thread.currentThread().sleep(100);
         assertNotNull("timed soft reference cleared too early", tsr.get());
         Thread.currentThread().sleep(100);
