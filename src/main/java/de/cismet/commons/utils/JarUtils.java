0441667714afe3a703e47966bfa2b5493838a713
Gilles Baatz <gilles.baatz@cismet.de>
2014-03-18 17:29:04 +0100

    #30 added the method unsignJar() to JarUtils

--- src/main/java/de/cismet/commons/utils/JarUtils.java
+++ src/main/java/de/cismet/commons/utils/JarUtils.java
@@ -9,16 +9,29 @@ package de.cismet.commons.utils;
 
 import org.apache.log4j.Logger;
 
+import org.openide.util.Exceptions;
+
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import java.net.URI;
+
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+
 import java.security.KeyStore;
 import java.security.PublicKey;
 import java.security.cert.Certificate;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.jar.JarInputStream;
@@ -58,6 +71,57 @@ public class JarUtils {
         } catch (java.util.zip.ZipException e) {
             // the uploaded file is NOT JAR file
             return false;
+        }
+    }
+
+    /**
+     * Removes the DSA, RSA and SF files from the META-INF folder from a zip file.
+     * 
+     * <p><b>Warning: Do not use with Java 6 or older.</b></p>
+     *
+     * @param  file  DOCUMENT ME!
+     */
+    public static void unsignJar(final File file) {
+        /* Define ZIP File System Properies in HashMap */
+        final Map<String, String> zip_properties = new HashMap<String, String>();
+        /* We want to read an existing ZIP File, so we set this to False */
+        zip_properties.put("create", "false");
+
+        /* Specify the path to the ZIP File that you want to read as a File System */
+        final URI zip_disk = URI.create("jar:file:" + file.getAbsolutePath());
+
+        /* Create ZIP file System */
+        FileSystem zipfs = null;
+        try {
+            zipfs = FileSystems.newFileSystem(zip_disk, zip_properties);
+            /* Get the Path inside ZIP File to delete the ZIP Entry */
+            final PathMatcher pathMatcher = zipfs.getPathMatcher("glob:*/META-INF/*.{DSA,SF,RSA}");
+
+            final Path metaInformationsDir = zipfs.getPath("META-INF/");
+
+            final DirectoryStream<Path> ds = Files.newDirectoryStream(metaInformationsDir);
+            for (final Path subPath : ds) {
+                if (pathMatcher.matches(subPath)) {
+                    if (LOG.isDebugEnabled()) {
+                        LOG.debug("About to delete an entry from ZIP File" + subPath.toUri());
+                    }
+                    /* Execute Delete */
+                    Files.delete(subPath);
+                    if (LOG.isDebugEnabled()) {
+                        LOG.debug("File successfully deleted");
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            LOG.error(ex.getMessage(), ex);
+        } finally {
+            try {
+                if (zipfs != null) {
+                    zipfs.close();
+                }
+            } catch (IOException ex) {
+                LOG.error(ex.getMessage(), ex);
+            }
         }
     }
 
