d13c1ce97fbbd457e98a5d7a95249e7974514912
JeanAtCismet <jean.ruiz@cismet.de>
2018-03-22 10:18:14 +0100

    Revert "Merge remote-tracking branch 'origin/release/6.2' into dev"
    This reverts commit 9d234d4fcce1d92b22384f3cd3a6e32fa422e7b1, reversing
    changes made to e93b0470f29134c12ab722a636bb91b469a343b7.

--- src/main/java/de/cismet/connectioncontext/ConnectionContext.java
+++ src/main/java/de/cismet/connectioncontext/ConnectionContext.java
@@ -18,27 +18,18 @@ package de.cismet.connectioncontext;
  * @author   jruiz
  * @version  $Revision$, $Date$
  */
-public class ConnectionContext extends AbstractConnectionContext {
+public class ConnectionContext extends AbstractConnectionContext<String> {
 
     //~ Constructors -----------------------------------------------------------
 
     /**
-     * Creates a new ConnectionContext object.
-     *
-     * @param  category  DOCUMENT ME!
-     */
-    protected ConnectionContext(final Category category) {
-        super(category);
-    }
-
-    /**
      * Creates a new ClientConnectionContext object.
      *
-     * @param  category     DOCUMENT ME!
-     * @param  contextName  DOCUMENT ME!
+     * @param  category  DOCUMENT ME!
+     * @param  content   DOCUMENT ME!
      */
-    protected ConnectionContext(final Category category, final String contextName) {
-        super(category, contextName);
+    protected ConnectionContext(final Category category, final String content) {
+        super(category, content);
     }
 
     //~ Methods ----------------------------------------------------------------
@@ -75,5 +66,14 @@ public class ConnectionContext extends AbstractConnectionContext {
      */
     public static ConnectionContext create(final Category category, final String context) {
         return new ConnectionContext(category, context);
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public Origin getOrigin() {
+        return Origin.CLIENT;
     }
 }
